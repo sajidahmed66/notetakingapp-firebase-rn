fbcf87555ac8865a044a2737d28168e481c0625b
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-11-01 19:04:25 +0600

    styled component for nallnotes screen

--- app/screens/AllNotes/index.tsx
+++ app/screens/AllNotes/index.tsx
@@ -1,17 +1,8 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  Dimensions,
-  StyleSheet,
-  Alert,
-  ScrollView,
-} from "react-native";
+import { Text, TouchableOpacity, Alert, ScrollView } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParam } from "../../navigation/RootVanigation";
-import Constants from "expo-constants";
-import styled from "styled-components/native";
+
 import {
   collection,
   getDocs,
@@ -22,20 +13,18 @@ import {
 import { app } from "../../../config/firebaseConfig";
 
 import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
+import {
+  Containter,
+  Fab,
+  HeaderText,
+  NoteCard,
+  TouchableTextTitle,
+} from "./styled";
+
 type Prop = NativeStackScreenProps<RootStackParam, "AllNotes">;
 
-const { width, height } = Dimensions.get("window");
-
-const Containter = styled.View`
-  flex: 1;
-  max-height: ${height}px;
-  padding-top: ${Constants.statusBarHeight}px;
-`;
-
-// const headerHeight = useHeaderHeight();
 const AllNote: React.FC<Prop> = ({ navigation }) => {
   const [notes, setNotes] = useState<any>();
-  const [reload, setReload] = useState<any>({});
   const db = getFirestore(app);
   const notesCollectionRef = collection(db, "notes");
 
@@ -72,18 +61,17 @@ const AllNote: React.FC<Prop> = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.headerText}>Note Lists</Text>
+    <Containter>
+      <HeaderText>Note Lists</HeaderText>
       <ScrollView>
         {notes?.length > 0 ? (
           notes.map((note: any) => (
-            <View key={note?.id} style={styles.notetitleCard}>
-              <TouchableOpacity
-                style={styles.cardTitle}
+            <NoteCard key={note?.id}>
+              <TouchableTextTitle
                 onPress={() => navigation.replace("ViewNote", { note })}
               >
                 <Text>{note.title}</Text>
-              </TouchableOpacity>
+              </TouchableTextTitle>
               <TouchableOpacity
                 onPress={() => {
                   deleteNoteHandler(note?.id);
@@ -91,80 +79,23 @@ const AllNote: React.FC<Prop> = ({ navigation }) => {
               >
                 <AntDesign name="delete" size={24} color="#251B37" />
               </TouchableOpacity>
-            </View>
+            </NoteCard>
           ))
         ) : (
-          <Text>No notes FOund</Text>
+          <Text>No notes Found</Text>
         )}
       </ScrollView>
       <TouchableOpacity onPress={() => navigation.replace("AddNote")}>
-        <View style={styles.fab}>
+        <Fab>
           <MaterialCommunityIcons
             name="note-text-outline"
             size={45}
             color="black"
           />
-        </View>
+        </Fab>
       </TouchableOpacity>
-    </View>
+    </Containter>
   );
 };
 
 export default AllNote;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    hight: height,
-    width: width,
-    // paddingTop: Constants.statusBarHeight,
-    // borderColor: "black",
-    // borderWidth: 2,
-    paddingHorizontal: 10,
-    position: "relative",
-    paddingTop: 20,
-    backgroundColor: "#FFF8EA",
-  },
-  headerText: {
-    height: 50,
-    fontSize: 18,
-    fontWeight: "bold",
-    paddingLeft: 10,
-  },
-  notetitleCard: {
-    maxWidthwidth: width,
-    height: 70,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    borderRadius: 15,
-    borderColor: "gray",
-    borderWidth: 2,
-    marginBottom: 10,
-    paddingHorizontal: 30,
-    backgroundColor: "#9E7676",
-  },
-  cardTitle: {
-    width: "75%",
-    height: "100%",
-    // borderColor: "black",
-    // borderWidth: 2,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-  fab: {
-    position: "absolute",
-    bottom: 20,
-    right: 30,
-    height: 70,
-    width: 70,
-    borderRadius: 35,
-    // borderWidth: 2,
-    backgroundColor: "#dee2e3",
-    overflow: "hidden",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ app/screens/AllNotes/styled.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+export const Containter = styled.View`
+  flex: 1;
+  /* height: height, */
+  width: ${width}px;
+  padding: 10px;
+  position: relative;
+  padding-top: 20px;
+  background-color: "#FFF8EA";
+`;
+
+export const HeaderText = styled.Text`
+  height: 50px;
+  font-size: 18px;
+  font-weight: bold;
+  padding-left: 10px;
+`;
+
+export const NoteCard = styled.View`
+  /* max-width: ${width}px; */
+  height: 70px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 15px;
+  border-color: gray;
+  border-width: 2px;
+  margin-bottom: 10px;
+  padding: 0px 30px;
+  background-color: #9e7676;
+`;
+
+export const TouchableTextTitle = styled.TouchableOpacity`
+  width: 75%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const Fab = styled.View`
+  position: absolute;
+  bottom: 20px;
+  right: 30px;
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  background-color: #dee2e3;
+  /* overflow: hidden; */
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
